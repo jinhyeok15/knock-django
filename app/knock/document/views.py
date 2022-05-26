1070dcf5ec138a6fe46adb45f19615a39679612a
jinhyeok15 <ekfrhd97@gmail.com>
2022-05-26 23:22:11 +0900

    feat: redirect 부분 추가

--- app/knock/document/views.py
+++ app/knock/document/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.datastructures import MultiValueDictKeyError
 
 # Create your views here.
 
 def index(request):
-    if request.GET['keyword-input']:
-        # location = generate_location(obj)
+    try:
+        keyword_input = request.GET['keyword-input']
+        if keyword_input:
+            # location = generate_location(obj)
+            return redirect('document-index')
+    except MultiValueDictKeyError as e:
         pass
+
     return render(request, 'document/index.html')
